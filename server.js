4331951c73346e2cd7cf52cc869d3a0b2a3992de
IT_MAC27 <arielyonzon10@gmail.com>
2025-10-15 15:29:55 +0800

    retrive endpoint

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();  // <-- create the Express app
 app.use(express.json());  
 app.use(cors());
 
+const MONGODB_URI= process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/test_yonzon'
+mongoose.connect(MONGODB_URI)
+  .then(() => console.log("connected to mongodb"))
+  .catch((err) => console.log("mongodb connection error: ", err))
 
 app.get('/', (req, res) => {
   res.json({message: 'Hello World - API is up!' });
